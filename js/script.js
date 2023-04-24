ad6c7572e8d4494abef2dd785cb08a9a975a48e0
Renan Lemos <90201009+renanleemos@users.noreply.github.com>
2023-04-24 11:51:26 -0300

    Finalizando Desktop

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".slide-content", {
+var swiper = new Swiper(".carousel", {
   slidesPerView: "auto",
   spaceBetween: 1,
   // loop: true,
@@ -27,12 +27,23 @@ var swiper = new Swiper(".slide-content", {
   },
 });
 
-$(document).ready(function(){
-    $("img").hover(function(){
-      $("#div1").fadeIn();
-      $("#div2").fadeIn();
-      $("#div3").fadeIn();
-    });
-  });
+const carouselItems = document.querySelectorAll(".carousel .item");
+const contentItems = document.querySelectorAll(".content .item");
 
+carouselItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    carouselItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+    contentItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+    const value = item.getAttribute("data-value");
+    const contentItem = document.querySelector(
+      `.content .item[data-value="${value}"]`
+    );
+    item.classList.add("active");
+    contentItem.classList.add("active");
+  });
+});
   
