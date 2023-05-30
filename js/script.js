21d9fa65f6671ca61f3e8eb8921f073238c0c120
Renan Lemos <90201009+renanleemos@users.noreply.github.com>
2023-05-30 16:35:25 -0300

    layout mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const carouselItems = document.querySelectorAll(".carousel .item");
 const contentItems = document.querySelectorAll(".content .item");
+const titleItems = document.querySelectorAll(".titleItem .item");
+const posItems = document.querySelectorAll(".pos__icons .item");
 
 carouselItems.forEach((item) => {
   item.addEventListener("mouseenter", () => {
@@ -9,11 +11,25 @@ carouselItems.forEach((item) => {
     contentItems.forEach((item) => {
       item.classList.remove("active");
     });
+    titleItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+    posItems.forEach((item) => {
+      item.classList.remove("active");
+    });
     const value = item.getAttribute("data-value");
     const contentItem = document.querySelector(
       `.content .item[data-value="${value}"]`
     );
+    const titleItem = document.querySelector(
+      `.titleItem .item[data-value="${value}"]`
+    );
+    const posItem = document.querySelector(
+      `.pos__icons .item[data-value="${value}"]`
+    );
     item.classList.add("active");
     contentItem.classList.add("active");
+    titleItem.classList.add("active");
+    posItem.classList.add("active");
   });
 });
